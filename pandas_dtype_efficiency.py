ceda42066c6df2e4c910c255c32df1641f09b1f2
gg-kieran <kieran@giffgaff.co.uk>
2021-05-03 11:34:40 +0100

    Add work in progress source code and tests

---
+++ pandas_dtype_efficiency.py
@@ -0,0 +1,228 @@
+"""
+Evaluating pandas DataFrames to see whether the data can be compressed in a lossless manner.
+"""
+
+# Standard libraries
+from typing import Dict, List, Union
+
+# Third party libraries
+import numpy as np
+import pandas as pd
+
+
+class DataFrameChecker:
+    """
+    Evaluate a pandas DataFrame to see whether its memory usage can be reduced whilst still preserving its data.
+    """
+
+    def __init__(self, df: pd.DataFrame, categorical_threshold: int = 15, float_size: int = 16):
+        """
+        Initialise checker with the DataFrame that needs to be evaluated.
+
+        Parameters
+        ----------
+        df : pandas DataFrame
+            Data to be checked to see whether any columns could reduce their memory usage.
+        categorical_threshold : int (default 15)
+            The maximum number of distinct values in a column of strings to suggest transforming it into a categorical
+            column.
+        float_size : int (default 16)
+            The desired numpy float type; 16: numpy float16, 32: numpy float23, 64: numpy float64 (the pandas default).
+        """
+
+        self._all_possible_improvements = {}
+        self._dataframe_has_been_analysed = False
+        self._df = df
+        self._categorical_threshold = categorical_threshold
+        self._columns_by_type = self._separate_dtypes()
+
+        if float_size not in [16, 32, 64]:
+            raise ValueError('float_precision must correspond to a numpy.float (one of 16, 32, or 64)')
+        self._float_size = float_size
+
+    def cast_dataframe_to_lower_memory_version(self) -> Union[pd.DataFrame, None]:
+        """
+        Take the original DataFrame and create a new one in which the appropriate columns have been cast to a
+        lower-memory data type.
+
+        Returns
+        -------
+        pandas DataFrame
+            Original data represented in lower-memory data types where possible.
+        """
+
+        if not self._dataframe_has_been_analysed:
+            raise UserWarning(
+                'DataFrame has not been analysed for improvements yet. Run `identify_possible_improvements` method '
+                'first. '
+            )
+
+        if not self._all_possible_improvements:
+            raise UserWarning('No improvements have been found after analysing DataFrame.')
+
+        lower_memory_df = self._df.astype(dtype=self._all_possible_improvements)
+
+        original_df_memory_size_bytes = self._df.memory_usage(deep=True).sum()
+        new_df_memory_size_bytes = lower_memory_df.memory_usage(deep=True).sum()
+
+        print(f'Original DataFrame memory: {(original_df_memory_size_bytes / 1024):,} megabytes')
+        print(f'New DataFrame memory: {(new_df_memory_size_bytes / 1024):,} megabytes')
+        return lower_memory_df
+
+    def _check_if_integer_sizes_can_be_reduced(self) -> Dict[str, type]:
+        """
+        Evaluate each integer column and see whether it can be reduced from int64 to a smaller integer type.
+
+        Returns
+        -------
+        dict
+            Mapping between column name and a reduced integer size which can accommodate its values.
+        """
+
+        # Range of values which different integer sizes can accommodate
+        integer_ranges = {
+            '8_min': -128, '8_max': 127,
+            '16_min': -32768, '16_max': 32767,
+            '32_min': -2147483648, '32_max': 2147483647
+        }
+
+        # Mapping between integer size and numpy type
+        integer_size_to_numpy_type = {
+            8: np.int8,
+            16: np.int16,
+            32: np.int32
+        }
+
+        possible_improvements = {}
+
+        int_columns = self._columns_by_type.get('int')
+
+        # Stop if no integer columns exist
+        if not int_columns:
+            return possible_improvements
+
+        # Check if column could be accommodated within a smaller integer size
+        for col in int_columns:
+            min_val = self._df[col].min()
+            max_val = self._df[col].max()
+
+            for int_size in [8, 16, 32]:
+                if min_val >= integer_ranges[f'{int_size}_min'] and max_val <= integer_ranges[f'{int_size}_max']:
+                    possible_improvements[col] = integer_size_to_numpy_type[int_size]
+                    break
+
+        return possible_improvements
+
+    def _check_if_strings_could_be_categorical(self) -> Dict[str, str]:
+        """
+        Evaluate each string column and see whether it contains a low number of distinct values, indicating it could
+        be represented as a categorical variable.
+
+        Returns
+        -------
+        dict
+            Any columns containing strings which could be transformed into a categorical variable.
+        """
+
+        possible_improvements = {}
+
+        string_columns = self._columns_by_type.get('object')
+
+        # Stop if no string columns exist
+        if not string_columns:
+            return possible_improvements
+
+        for col in string_columns:
+            if self._df[col].nunique() <= self._categorical_threshold:
+                possible_improvements[col] = 'category'
+
+        return possible_improvements
+
+    def _flag_float_column_improvements(self) -> Dict[str, type]:
+        """
+        Map each float column and how it could be represented in a lower precision.
+
+        Returns
+        -------
+        dict
+            Any float columns which could be reduced to the desired level of precision.
+        """
+
+        possible_improvements = {}
+
+        # No need to map if desired float size is the pandas default (64)
+        if self._float_size == 64:
+            return possible_improvements
+
+        # Mapping between desired float size and numpy type
+        float_size_to_numpy_type = {
+            16: np.float16,
+            32: np.float32
+        }
+
+        float_columns = self._columns_by_type.get('float')
+
+        # Stop if no float columns exist
+        if not float_columns:
+            return possible_improvements
+
+        for col in float_columns:
+            if self._df[col].nunique() <= self._categorical_threshold:
+                possible_improvements[col] = float_size_to_numpy_type[self._float_size]
+
+        return possible_improvements
+
+    def get_possible_dtypes(self) -> Dict[str, type]:
+        """
+        Retrieve a dictionary containing any columns with the potential for reduced memory, and the dtype that can be
+        used to represent them in a more efficient manner.
+
+        Returns
+        -------
+        dict
+            Any columns with the potential for reduced memory, and the dtype that can be used to represent them in a
+            more efficient manner.
+        """
+
+        return self._all_possible_improvements
+
+    def identify_possible_improvements(self) -> None:
+        """
+        Analyse the dataframe and store a dictionary containing any column with the potential for reduced memory, and
+        the dtype that can be used to represent it in a more efficient manner.
+        """
+
+        print('Analysing any float columns')
+        float_improvements = self._flag_float_column_improvements()
+        self._all_possible_improvements.update(float_improvements)
+
+        print('Analysing any integer columns')
+        integer_improvements = self._check_if_integer_sizes_can_be_reduced()
+        self._all_possible_improvements.update(integer_improvements)
+
+        print('Analysing any string columns')
+        categorical_improvements = self._check_if_strings_could_be_categorical()
+        self._all_possible_improvements.update(categorical_improvements)
+
+        self._dataframe_has_been_analysed = True
+
+    def _separate_dtypes(self) -> Dict[str, List[str]]:
+        """
+        Create a mapping between data types and the columns that belong to that data type. Only data types which have
+        the potential for reduced memory usage are included.
+
+        Returns
+        -------
+        dict
+            Mapping between data type and the columns which correspond to that data type.
+        """
+
+        columns_by_dtype = {}
+
+        for data_type in ['float', 'int', 'object']:
+            relevant_columns = list(self._df.select_dtypes(include=data_type).columns)
+
+            if relevant_columns:
+                columns_by_dtype[data_type] = relevant_columns
+
+        return columns_by_dtype
---
+++ tests/test_pandas_dtype_efficiency.py
@@ -0,0 +1,141 @@
+# Third party libraries
+import numpy as np
+import pandas as pd
+import pytest
+
+# Internal imports
+import pandas_dtype_efficiency as pd_eff
+
+# Mock data for consistent evaluation
+MOCK_DF = pd.DataFrame(
+    data={
+        'floats': [-0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0.5],
+        'int_smaller_than_int8': [-128, 0, 0, 0, 0, 0, 0, 0, 0, 127],
+        'int_smaller_than_int16': [-32768, 0, 0, 0, 0, 0, 0, 0, 0, 32767],
+        'int_smaller_than_int32': [-2147483648, 0, 0, 0, 0, 0, 0, 0, 0, 2147483647],
+        'int_smaller_than_int64': [-9223372036854775808, 0, 0, 0, 0, 0, 0, 0, 0, 9223372036854775807],
+        'category_strings': ['C1', 'C2', 'C1', 'C2', 'C1', 'C2', 'C1', 'C2', 'C1', 'C2'],
+        'varied_strings': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'],
+        'ineligible_column': [True, False, True, False, True, False, True, False, True, False]
+    }
+)
+
+
+@pytest.fixture
+def checker() -> pd_eff.DataFrameChecker:
+    """
+    Establish a consistent checker with the same mock data.
+    """
+
+    return pd_eff.DataFrameChecker(df=MOCK_DF)
+
+
+class TestDataFrameChecker:
+    """
+    Tests for DataFrameChecker.
+    """
+
+    def test_cast_dataframe_to_lower_memory_version(self, checker):
+        """
+        DataFrame has the appropriate columns converted to new data types.
+        """
+
+        expected_output = pd.Series(
+            data={
+                'floats': np.dtype('float16'),
+                'int_smaller_than_int8': np.dtype('int8'),
+                'int_smaller_than_int16': np.dtype('int16'),
+                'int_smaller_than_int32': np.dtype('int32'),
+                'int_smaller_than_int64': np.dtype('int64'),
+                'category_strings': 'category',
+                'varied_strings': 'category',
+                'ineligible_column': np.dtype('bool')
+            }
+        )
+
+        checker.identify_possible_improvements()
+        lower_memory_df = checker.cast_dataframe_to_lower_memory_version()
+        lower_memory_df_dtypes = lower_memory_df.dtypes
+
+        pd.testing.assert_series_equal(left=lower_memory_df_dtypes, right=expected_output)
+
+    def test__check_if_integer_sizes_can_be_reduced(self, checker):
+        """
+        Possible improvements to the integer size of each column are correctly found.
+        """
+
+        expected_output = {
+            'int_smaller_than_int8': np.int8,
+            'int_smaller_than_int16': np.int16,
+            'int_smaller_than_int32': np.int32
+        }
+
+        assert checker._check_if_integer_sizes_can_be_reduced() == expected_output
+
+    @pytest.mark.parametrize(
+        argnames='categorical_threshold, expected_output',
+        argvalues=[
+            (5, {'category_strings': 'category'}),
+            (15, {'category_strings': 'category', 'varied_strings': 'category'})
+        ]
+    )
+    def test__check_if_strings_could_be_categorical(self, categorical_threshold, expected_output):
+        """
+        Possible categorical representations of string columns are found.
+        """
+
+        # Overwrite default
+        checker = pd_eff.DataFrameChecker(df=MOCK_DF, categorical_threshold=categorical_threshold)
+
+        assert checker._check_if_strings_could_be_categorical() == expected_output
+
+    @pytest.mark.parametrize(
+        argnames=('float_size', 'expected_output'),
+        argvalues=[(16, {'floats': np.float16}), (32, {'floats': np.float32}), (64, {})]
+    )
+    def test__flag_float_column_improvements(self, float_size, expected_output):
+        """
+        Possible float columns are marked for a lower precision representation, or ignored if the default pandas value
+        of 64 is requested.
+        """
+
+        # Overwrite default
+        checker = pd_eff.DataFrameChecker(df=MOCK_DF, float_size=float_size)
+
+        assert checker._flag_float_column_improvements() == expected_output
+
+    def test_get_potential_dtypes(self, checker):
+        """
+        All possible improvements are compiled into a single dictionary.
+        """
+
+        checker.identify_possible_improvements()
+
+        expected_output = {
+            'floats': np.float16,
+            'int_smaller_than_int8': np.int8,
+            'int_smaller_than_int16': np.int16,
+            'int_smaller_than_int32': np.int32,
+            'category_strings': 'category',
+            'varied_strings': 'category'
+        }
+
+        assert checker.get_possible_dtypes() == expected_output
+
+    def test__separate_dtypes(self, checker):
+        """
+        Columns are correctly recorded in terms of how they should be checked when seeing whether their memory usage
+        could be reduced.
+        """
+
+        checker._separate_dtypes()
+
+        expected_output = {
+            'float': ['floats'],
+            'int': [
+                'int_smaller_than_int8', 'int_smaller_than_int16', 'int_smaller_than_int32', 'int_smaller_than_int64'
+            ],
+            'object': ['category_strings', 'varied_strings']
+        }
+
+        assert checker._separate_dtypes() == expected_output
