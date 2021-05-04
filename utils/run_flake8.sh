547d9e17935f533e4476afcc2af76f6ba52518a6
gg-kieran <kieran@giffgaff.co.uk>
2021-05-04 09:14:12 +0100

    Place long bash commands over multiple lines

--- utils/run_flake8.sh
+++ utils/run_flake8.sh
@@ -1,1 +1,2 @@
-docker exec pandas_dtype_efficiency_dev /home/docker_user/.local/bin/flake8 pandas_dtype_efficiency.py --max-line-length 120
+docker exec pandas_dtype_efficiency_dev /home/docker_user/.local/bin/flake8 pandas_dtype_efficiency.py \
+  --max-line-length 120
--- utils/run_linter.sh
+++ utils/run_linter.sh
@@ -1,1 +1,2 @@
-docker exec pandas_dtype_efficiency_dev /home/docker_user/.local/bin/pylint pandas_dtype_efficiency.py --max-line-length 120
+docker exec pandas_dtype_efficiency_dev /home/docker_user/.local/bin/pylint pandas_dtype_efficiency.py \
+  --max-line-length 120
